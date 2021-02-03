163a560f53f28f4a4a945b4ac445c451f8e55a19
Ryan Erricson <ryanerricson@gmail.com>
2021-02-03 13:23:39 -0500

    commit tree ecf4e0524be92927c1aaa461735ef8d970a4f227

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    batch_badge = []
+    array.each do |name|
+        batch_badge << "Hello, my name is #{name}."
+    end
+    return batch_badge
+end
+
+def assign_rooms(array)
+    room_assign = []
+    array.each_with_index do |name, index|
+        room_assign << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    return room_assign
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |name|
+        puts name
+    end
+    assign_rooms(attendees).each do |name|
+        puts name
+    end
+end
